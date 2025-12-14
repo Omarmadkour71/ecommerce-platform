3fb8c5bb0f800f548437cac099a182a61f8e28c2
Omarmadkour71 <omarmadkour71@gmail.com>
2025-12-14 20:57:20 +0200

    Setting up cookies

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,6 +14,18 @@ const creatSendToken = (user, statusCode, res) => {
     expiresIn: process.env.JWT_EXPIRE_IN,
   });
 
+  // Setting up Cookie Options and sending token via Cookie
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+  if (process.env.NODE_ENV === "production") {
+    cookieOptions.secure = true;
+  }
+  res.cookie("jwt", token, cookieOptions);
+
   // Sending Response to user
   user.password = undefined;
   res.status(statusCode).json({
